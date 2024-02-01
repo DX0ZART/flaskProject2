17cb7319755e904b971922c5789a744d970ea164
dx0zart <evanyifanz@gmail.com>
2024-02-01 17:25:20 -0500

    1/31

--- app.py
+++ app.py
@@ -1,9 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
+
+class User:
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
+
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World!'
+   user = User('John', 'xx@qq.com')
+    return render_template('index.html')
+
+
+@app.route('/blog/<blog_id>')
+def blog_detail(blog_id):
+    return render_template("blog_detail.html", blog_id=blog_id)
+
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True, host='0.0.0.0', port=5000)
